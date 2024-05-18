fdfe2ea24de229524cba7b77e9ec807ab26b5961
Oliver Malate <oliver95malate@gmail.com>
2024-05-19 04:24:31 +0800

    navigation customization commit 4

---
+++ script.js
@@ -0,0 +1,10 @@
+function closeMenu (){
+    const menuItems = document.querySelectorAll('.menu li a');
+    const checkBox = document.getElementById('check')
+
+    menuItems.forEach(items => {
+        items.addEventListener('click', () => {
+            checkBox.checked = false
+        })
+    })
+}
