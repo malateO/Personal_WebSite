110ffac914818087bd2d9df8360ab9bdbc5d3627
Oliver Malate <oliver95malate@gmail.com>
2024-05-28 04:12:48 +0800

    new navigation

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-function closeMenu() {
-  const menuItems = document.querySelectorAll(".menu li a");
-  const checkBox = document.getElementById("check");
-
-  menuItems.forEach((items) => {
-    items.addEventListener("click", () => {
-      checkBox.checked = false;
-    });
-  });
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
 }
-
-closeMenu();
 
 const iconHoverColor = () => {
   const faBrands = document.querySelectorAll(".fa-brands");
